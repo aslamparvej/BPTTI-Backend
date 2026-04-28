ebce318838468159303f0014eeb9ae7c63e515a8
Aslam Parvej <aslamparvej2@gmail.com>
2026-04-28 10:12:19 +0530

    Added upload pfd functionality

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 const authRoutes = require("./routes/authRoutes");
+const pdfRoutes = require("./routes/pdfRoutes");
 
 const app = express();
 
@@ -10,6 +11,8 @@ app.use(express.json());
 
 // Routes
 app.use("/api/auth", authRoutes);
+app.use("/api/pdfs", pdfRoutes);
+
 app.get("/", (req, res) => {
     res.send("Welcome to the Bhagirathi Primary Teachers Training Institute API!");
 });
